6c420f9bc60816b6fec5c8219ff7425d7276b389
Hao Wu <wuhao@cocos2d-x.org>
2012-11-15 10:07:11 +0800

    playable demo

---
+++ watermelon_with_me/resources-html5.js
@@ -0,0 +1,70 @@
+var s_coinsPlist = 'Resources/CCB/coins.plist';
+var s_coinsAnimation = 'Resources/coins_animation.plist';
+var s_parallax = 'Resources/single_files/Parallax.png';
+var s_carSmoke = 'Resources/car_smoke.plist';
+var s_HUD = "Resources/CCB/HUD.ccbi";
+
+var s_Abadi40HDFNT = "Resources/CCB/Abadi40-hd.fnt";
+var s_Abadi40HDPNG = "Resources/CCB/Abadi40-hd.png";
+
+var s_Abadi40IPADFNT = "Resources/CCB/Abadi40-ipad.fnt";
+var s_Abadi40IPADPNG = "Resources/CCB/Abadi40-ipad.png";
+
+var s_Abadi40FNT = "Resources/CCB/Abadi40.fnt";
+var s_Abadi40PNG = "Resources/CCB/Abadi40.png";
+
+
+var s_Gas40FNT = "Resources/CCB/Gas40.fnt";
+var s_Gas40PNG = "Resources/CCB/Gas40.png";
+
+var s_Gas40HDFNT = "Resources/CCB/Gas40-hd.fnt";
+var s_Gas40HDPNG = "Resources/CCB/Gas40-hd.png";
+
+var s_Gas40IPADFNT = "Resources/CCB/Gas40-ipad.fnt";
+var s_Gas40IPADPNG = "Resources/CCB/Gas40-ipad.png";
+
+
+var s_Konqa32FNT = "Resources/CCB/konqa32.fnt";
+var s_Konqa32PNG = "Resources/CCB/konqa32.PNG";
+
+var s_Konqa32HDFNT = "Resources/CCB/konqa32-hd.fnt";
+var s_Konqa32HDPNG = "Resources/CCB/konqa32-hd.PNG";
+
+var s_Konqa32IPADFNT = "Resources/CCB/konqa32-ipad.fnt";
+var s_Konqa32IPADPNG = "Resources/CCB/konqa32-ipad.PNG";
+
+
+var s_GameOverWAV = "Resources/GameOver.wav";
+var PickupCointWAV = "Resources/pickup_coin.wav";
+var s_LevelComplete = "Resources/LevelComplete.wav";
+
+var g_ressources = [
+    {type:'plist', src:s_coinsPlist},
+    {type:'plist', src:s_coinsAnimation},
+    {type:'plist', src:s_carSmoke},
+    {type:'fnt', src:s_Abadi40HDFNT},
+    {type:'fnt', src:s_Abadi40IPADFNT},
+    {type:'fnt', src:s_Abadi40FNT},
+    {type:'image', src:s_Abadi40HDPNG},
+    {type:'image', src:s_Abadi40IPADPNG},
+    {type:'image', src:s_Abadi40PNG},
+    {type:'fnt', src:s_Gas40FNT},
+    {type:'fnt', src:s_Gas40HDFNT},
+    {type:'fnt', src:s_Gas40IPADFNT},
+    {type:'image', src:s_Gas40PNG},
+    {type:'image', src:s_Gas40HDPNG},
+    {type:'image', src:s_Gas40IPADPNG},
+    {type:'fnt', src:s_Konqa32FNT},
+    {type:'fnt', src:s_Konqa32HDFNT},
+    {type:'fnt', src:s_Konqa32IPADFNT},
+    {type:'image', src:s_Konqa32PNG},
+    {type:'image', src:s_Konqa32HDPNG},
+    {type:'image', src:s_Konqa32IPADPNG},
+    {type:'effect', src:s_GameOverWAV},
+    {type:'effect', src:PickupCointWAV},
+    {type:'effect', src:s_LevelComplete},
+
+    {type:'image', src:"Resources/CCB/Gas40.png"},
+    {type:'image', src:s_parallax},
+    {type: 'ccbi', src:s_HUD}
+];
