525a86d2af99aac5f470726292a0c230afab6448
lzn <zhenning.liu@cocos2d-x.org>
2012-11-27 14:42:16 +0800

    change to English

--- src/tests/ExtensionsTest/EditBoxTest/EditBoxTestScene.js
+++ src/tests/ExtensionsTest/EditBoxTest/EditBoxTestScene.js
@@ -1,3 +1,29 @@
+/****************************************************************************
+ Copyright (c) 2010-2012 cocos2d-x.org
+ Copyright (c) 2008-2010 Ricardo Quesada
+ Copyright (c) 2011      Zynga Inc.
+
+ http://www.cocos2d-x.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
 var runEditBoxTest = function () {
 
     var pScene = cc.Scene.create();
@@ -14,38 +40,38 @@ var EditBoxTestLayer = cc.Layer.extend({
         var box1 = cc.ControlEditBox.create(200,30,16);
         box1.setDefaultValue("EditBoxs");
         box1.setPosition(220,50);
-        box1.setBgClr("#ff6600");
+        box1.setBgClr("darkorange");
         box1.setFontColor("black");
-        box1.setBorderClr("#ccddff");
+        box1.setBorderClr("azure");
         box1.setZIndex(10);
         this.addChild(box1);
 
         var box2 = cc.ControlEditBox.create(220,25,10);
-        box2.setDefaultValue("汉字输入法EditBox Sample");
+        box2.setDefaultValue("EditBox Sample");
         box2.setPosition(220,150);
         box2.setPasswordStyle();
-        box2.setBgClr("#f066f0");
-        box2.setFontColor("green");
-        box2.setBorderClr("#eeff00");
+        box2.setBgClr("lightgreen");
+        box2.setFontColor("orange");
+        box2.setBorderClr("coral");
         box2.setZIndex(10);
         this.addChild(box2);
 
         var box3 = cc.ControlEditBox.create(76,40,10);
-        box3.setDefaultValue("背景样式");
+        box3.setDefaultValue("ImageStyle");
         box3.setPosition(220,250);
-        box3.setBgClr("#e3fff0");
-        box3.setFontColor("blue");
-        box3.setBorderClr("#e3330f");
+        box3.setBgClr("salmon");
+        box3.setFontColor("indigo");
+        box3.setBorderClr("teal");
         box3.setZIndex(10);
         box3.setImgStyle("res/extensions/orange_edit.png");
         this.addChild(box3);
 
         var box4 = cc.ControlEditBox.create(240,35,14);
-        box4.setDefaultValue("限制输入的字数10个");
+        box4.setDefaultValue("input-limited 10");
         box4.setPosition(220,350);
-        box4.setBgClr("#996600");
-        box4.setFontColor("white");
-        box4.setBorderClr("#ffff99");
+        box4.setBgClr("bisque");
+        box4.setFontColor("black");
+        box4.setBorderClr("skyblue");
         box4.setZIndex(10);
         box4.setMaxLength(10);
         this.addChild(box4);
