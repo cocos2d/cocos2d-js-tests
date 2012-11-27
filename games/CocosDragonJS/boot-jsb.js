f8f7e5dc95909b5ca33c1d50ac9f883936983334
dingpinglv <dingping.lv@cocos2d-x.org>
2012-11-27 15:41:24 +0800

    issue #1579 add Resources and src to CocosDragonJS

---
+++ games/CocosDragonJS/boot-jsb.js
@@ -0,0 +1,13 @@
+/*
+ Watermelon With Me
+ Boot file for JSB
+ */
+
+// JS Bindings constants
+require("jsb_constants.js");
+
+// resource file
+require('resources-jsb.js');
+
+// game file
+require('watermelon_with_me.js');
---
+++ games/CocosDragonJS/resources-jsb.js
@@ -0,0 +1,2 @@
+var s_coinEffect = 'Resources/sound/Coin.mp3';
+var s_exploEffect = 'Resources/sound/Explo.mp3';
