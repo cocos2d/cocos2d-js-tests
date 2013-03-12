180ce25f915330f91d60a299e46c0802f00b571c
folecr <folecr@gmail.com>
2013-03-12 15:09:34 -0700

    Automate ParallaxTest

--- tests/ParallaxTest/ParallaxTest.js
+++ tests/ParallaxTest/ParallaxTest.js
@@ -71,70 +71,94 @@ ParallaxDemo = BaseTestLayer.extend({
 
 Parallax1 = ParallaxDemo.extend({
 
-    _root:null,
-    _target:null,
-    _streak:null,
+    _parent:null,
+    _background:null,
+    _tilemap:null,
+    _cocosimage:null,
 
     ctor:function () {
         this._super();
 
         // Top Layer, a simple image
-        var cocosImage = cc.Sprite.create(s_power);
+        _cocosimage = cc.Sprite.create(s_power);
         // scale the image (optional)
-        cocosImage.setScale(1.5);
+        _cocosimage.setScale(1.5);
         // change the transform anchor point to 0,0 (optional)
-        cocosImage.setAnchorPoint(cc.p(0, 0));
+        _cocosimage.setAnchorPoint(cc.p(0, 0));
 
 
         // Middle layer: a Tile map atlas
         //var tilemap = cc.TileMapAtlas.create(s_tilesPng, s_levelMapTga, 16, 16);
-        var tilemap = cc.TMXTiledMap.create(s_resprefix + "TileMaps/orthogonal-test2.tmx");
-
+        _tilemap = cc.TMXTiledMap.create(s_resprefix + "TileMaps/orthogonal-test2.tmx");
 
         // change the transform anchor to 0,0 (optional)
-        tilemap.setAnchorPoint(cc.p(0, 0));
+        _tilemap.setAnchorPoint(cc.p(0, 0));
 
         // Anti Aliased images
         //tilemap.getTexture().setAntiAliasTexParameters();
 
         // background layer: another image
-        var background = cc.Sprite.create(s_back);
+        _background = cc.Sprite.create(s_back);
         // scale the image (optional)
         //background.setScale(1.5);
         // change the transform anchor point (optional)
-        background.setAnchorPoint(cc.p(0, 0));
+        _background.setAnchorPoint(cc.p(0, 0));
 
 
         // create a void node, a parent node
-        var voidNode = cc.ParallaxNode.create();
+        _parent = cc.ParallaxNode.create();
 
         // NOW add the 3 layers to the 'void' node
 
         // background image is moved at a ratio of 0.4x, 0.5y
-        voidNode.addChild(background, -1, cc.p(0.4, 0.5), cc.p(0,0));
+        _parent.addChild(_background, -1, cc.p(0.4, 0.5), cc.p(0,0));
 
         // tiles are moved at a ratio of 2.2x, 1.0y
-        voidNode.addChild(tilemap, 1, cc.p(2.2, 1.0), cc.p(0, 0));
+        _parent.addChild(_tilemap, 1, cc.p(2.2, 1.0), cc.p(0, 0));
 
         // top image is moved at a ratio of 3.0x, 2.5y
-        voidNode.addChild(cocosImage, 2, cc.p(3.0, 2.5), cc.p(0, 0));
+        _parent.addChild(_cocosimage, 2, cc.p(3.0, 2.5), cc.p(0, 0));
 
 
         // now create some actions that will move the 'void' node
         // and the children of the 'void' node will move at different
         // speed, thus, simulation the 3D environment
-        var goUp = cc.MoveBy.create(4, cc.p(0, 100));
-        var goDown = goUp.reverse();
-        var go = cc.MoveBy.create(8, cc.p(200, 0));
-        var goBack = go.reverse();
-        var seq = cc.Sequence.create(goUp, go, goDown, goBack);
-        voidNode.runAction((cc.RepeatForever.create(seq) ));
+        var goUp = cc.MoveBy.create(1, cc.p(0, 100));
+        var goRight = cc.MoveBy.create(1, cc.p(200, 0));
+        var seq = cc.Sequence.create(goUp, goRight);
+        _parent.runAction(seq);
 
-        this.addChild(voidNode);
+        this.addChild(_parent);
     },
 
     title:function () {
         return "Parallax: parent and 3 children";
+    },
+
+    // default values for automation
+    testDuration:3,
+    getExpectedResult:function() {
+        var ret = {};
+        ret.pos_parent = cc.p(200,100);
+        ret.pos_child1 = cc.p(-120, -50);
+        ret.pos_child2 = cc.p(240, 0);
+        ret.pos_child3 = cc.p(400, 150);
+
+        cc.log(JSON.stringify(ret));
+
+        return JSON.stringify(ret);
+    },
+
+    getCurrentResult:function() {
+        var ret = {};
+        ret.pos_parent = cc.p(Math.round(_parent.getPosition().x), Math.round(_parent.getPosition().y));
+        ret.pos_child1 = cc.p(Math.round(_background.getPosition().x), Math.round(_background.getPosition().y));;
+        ret.pos_child2 = cc.p(Math.round(_tilemap.getPosition().x), Math.round(_tilemap.getPosition().y));
+        ret.pos_child3 = cc.p(Math.round(_cocosimage.getPosition().x), Math.round(_cocosimage.getPosition().y));
+
+        cc.log(JSON.stringify(ret));
+
+        return JSON.stringify(ret);
     }
 });
 
