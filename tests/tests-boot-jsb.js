9b8a87472a90cf8b99915b14c3fca98e9d643f03
ShengxiangChen <shengxiang.chen@cocos2d-x.org>
2013-03-21 10:21:28 +0800

    obfuscate for jsb

--- tests/tests-boot-jsb.js
+++ tests/tests-boot-jsb.js
@@ -1,68 +1,71 @@
-
 // JS Bindings constants
 require("jsb.js");
 
-// Resources definitions
-require("tests_resources-jsb.js");
-
-// Load main file
-require("tests-main.js");
-
 // Load tests files
-
 var tests_files = [
+    // Resources definitions
+    'tests_resources-jsb.js',
 
-		// base class
-		'BaseTestLayer/BaseTestLayer.js',
+    // Load main file
+    'tests-main.js',
 
-		'ActionManagerTest/ActionManagerTest.js',
-		'ActionsTest/ActionsTest.js',
-		'ChipmunkTest/ChipmunkTest.js',
-		'ClickAndMoveTest/ClickAndMoveTest.js',
-		'CocosDenshionTest/CocosDenshionTest.js',
-		'CocosNodeTest/CocosNodeTest.js',
-		'DrawPrimitivesTest/DrawPrimitivesTest.js',
-		'EaseActionsTest/EaseActionsTest.js',
-		'EffectsTest/EffectsTest.js',
-		'EventTest/EventTest.js',
-		'FileUtils/FileUtilsTest.js',
-		'FontTest/FontTest.js',
-		'IntervalTest/IntervalTest.js',
-		'LabelTest/LabelTest.js',
-		'LayerTest/LayerTest.js',
-		'MenuTest/MenuTest.js',
-		'OpenGLTest/OpenGLTest.js',
-		'ParallaxTest/ParallaxTest.js',
-		'ParticleTest/ParticleTest.js',
-		'PerformanceTest/PerformanceTest.js',
-		'PerformanceTest/PerformanceNodeChildrenTest.js',
-		'PerformanceTest/PerformanceParticleTest.js',
-		'PerformanceTest/PerformanceSpriteTest.js',
-		'PerformanceTest/PerformanceSpriteTest2.js',
-		'PerformanceTest/PerformanceTextureTest.js',
-		'PerformanceTest/PerformanceTouchesTest.js',
-		'PerformanceTest/PerformanceAnimationTest.js',
-		'PerformanceTest/seedrandom.js',
-		'Presentation/Presentation.js',
-		'ProgressActionsTest/ProgressActionsTest.js',
-		'RenderTextureTest/RenderTextureTest.js',
-		'RotateWorldTest/RotateWorldTest.js',
-		'SceneTest/SceneTest.js',
-		'SchedulerTest/SchedulerTest.js',
-		'SpriteTest/SpriteTest.js',
-		'ExtensionsTest/S9SpriteTest/S9SpriteTest.js',
-		'TileMapTest/TileMapTest.js',
-		'TransitionsTest/TransitionsTest.js',
-		'UnitTest/UnitTest.js',
-		'SysTest/SysTest.js'
-	];
-for( var i=0; i < tests_files.length; i++) {
-	var name = "" + tests_files[i];
-	cc.log(" Loading: " + name );
-	require( name );
+    // base class
+    'BaseTestLayer/BaseTestLayer.js',
+
+    'ActionManagerTest/ActionManagerTest.js',
+    'ActionsTest/ActionsTest.js',
+    'ChipmunkTest/ChipmunkTest.js',
+    'ClickAndMoveTest/ClickAndMoveTest.js',
+    'CocosDenshionTest/CocosDenshionTest.js',
+    'CocosNodeTest/CocosNodeTest.js',
+    'DrawPrimitivesTest/DrawPrimitivesTest.js',
+    'EaseActionsTest/EaseActionsTest.js',
+    'EffectsTest/EffectsTest.js',
+    'EventTest/EventTest.js',
+    'FileUtils/FileUtilsTest.js',
+    'FontTest/FontTest.js',
+    'IntervalTest/IntervalTest.js',
+    'LabelTest/LabelTest.js',
+    'LayerTest/LayerTest.js',
+    'MenuTest/MenuTest.js',
+    'OpenGLTest/OpenGLTest.js',
+    'ParallaxTest/ParallaxTest.js',
+    'ParticleTest/ParticleTest.js',
+    'PerformanceTest/PerformanceTest.js',
+    'PerformanceTest/PerformanceNodeChildrenTest.js',
+    'PerformanceTest/PerformanceParticleTest.js',
+    'PerformanceTest/PerformanceSpriteTest.js',
+    'PerformanceTest/PerformanceSpriteTest2.js',
+    'PerformanceTest/PerformanceTextureTest.js',
+    'PerformanceTest/PerformanceTouchesTest.js',
+    'PerformanceTest/PerformanceAnimationTest.js',
+    'PerformanceTest/seedrandom.js',
+    'Presentation/Presentation.js',
+    'ProgressActionsTest/ProgressActionsTest.js',
+    'RenderTextureTest/RenderTextureTest.js',
+    'RotateWorldTest/RotateWorldTest.js',
+    'SceneTest/SceneTest.js',
+    'SchedulerTest/SchedulerTest.js',
+    'SpriteTest/SpriteTest.js',
+    'ExtensionsTest/S9SpriteTest/S9SpriteTest.js',
+    'TileMapTest/TileMapTest.js',
+    'TransitionsTest/TransitionsTest.js',
+    'UnitTest/UnitTest.js',
+    'SysTest/SysTest.js'
+];
+
+//whether to obfuscate the code
+var obfuscate = true;
+
+if (!obfuscate) {
+    for (var i = 0; i < tests_files.length; i++) {
+        var name = "" + tests_files[i];
+        cc.log(" Loading: " + name);
+        require(name);
+    }
 }
 
 var scene = cc.Scene.create();
 var layer = new TestController();
 scene.addChild(layer);
-director.runWithScene( scene );
+director.runWithScene(scene);
