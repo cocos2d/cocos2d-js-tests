999fabb306cddffd0c043ee1cff6e8627ac0df24
James Chen <jianhua.chen@cocos2d-x.org>
2013-03-22 15:11:17 +0800

    Including constants js files in jsb.js.

--- games/MoonWarriors/MoonWarriors-jsb.js
+++ games/MoonWarriors/MoonWarriors-jsb.js
@@ -26,6 +26,7 @@
 // boot code needed for cocos2d + JS bindings.
 // Not needed by cocos2d-html5
 
+require("jsb_cocos2d_constants.js");
 require("jsb_cocos2d.js");
 require("jsb_opengl.js");
 require("jsb_sys.js");
