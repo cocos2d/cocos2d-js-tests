5d769758489f0428a2d16f4a06473636fedf5bca
xingsenma <xingsen.ma@cocos2d-x.org>
2014-02-27 15:25:43 +0800

    issue #3945 cc.c4b -> cc.color

--- tests/ParticleTest/ParticleTest.js
+++ tests/ParticleTest/ParticleTest.js
@@ -177,7 +177,7 @@ var ParticleDemo = BaseTestLayer.extend({
     },
 
     ctor:function () {
-        this._super(cc.c4b(0,0,0,255), cc.c4b(98,99,117,255));
+        this._super(cc.color(0,0,0,255), cc.color(98,99,117,255));
         this._emitter = null;
 
         if ('touches' in sys.capabilities){
